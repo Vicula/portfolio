de46700362071af65368ac147cc7551e543a6ed6
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-13 13:46:01 -0500

    starting on the project sections

--- src/scripts/home-view.js
+++ src/scripts/home-view.js
@@ -287,12 +287,14 @@ const HomeView = React.createClass({
           </div>
         </div>
         <div className="projectBox">
-          <h1>Projects</h1>
-          <p></p>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <h1>PROJECTS</h1>
+          <p>These are a few of my projects that i am currently working on or have already completed. Each project uses different Javascript libraries and other frameworks that i use. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
         <div className="aboutMeBox">
           <h1>Approach</h1>
