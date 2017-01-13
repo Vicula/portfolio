223a18cb39c2eeb691fdf26b0fbff055c337aa1c
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-13 11:28:03 -0500

    added a google font

--- src/scripts/home-view.js
+++ src/scripts/home-view.js
@@ -71,7 +71,7 @@ const HomeView = React.createClass({
         </nav>
         <div className="jumboHeader">
           <img className="headerBackPic" src="../images/photo-1432821596592-e2c18b78144f.jpeg"/>
-          <h1 className="headerText">Front-End Developer Working for you</h1>
+          <h1 className="headerText">Front-End Developer <span className="headerLowerText">Working for you</span></h1>
         </div>
         <div className="skillsBox">
           <div></div>
