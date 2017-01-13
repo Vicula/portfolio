569db0b5b26aa02fa75785b8ceea2b356965a5a6
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-13 11:19:39 -0500

    getting it done

--- src/scripts/home-view.js
+++ src/scripts/home-view.js
@@ -71,6 +71,7 @@ const HomeView = React.createClass({
         </nav>
         <div className="jumboHeader">
           <img className="headerBackPic" src="../images/photo-1432821596592-e2c18b78144f.jpeg"/>
+          <h1 className="headerText">Front-End Developer Working for you</h1>
         </div>
         <div className="skillsBox">
           <div></div>
