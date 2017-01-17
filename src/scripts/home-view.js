a6ebafd11af30db7b68b33beead1f7a182fd5325
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-17 08:41:04 -0500

    added the map

--- src/scripts/home-view.js
+++ src/scripts/home-view.js
@@ -333,8 +333,16 @@ const HomeView = React.createClass({
         </div>
         <div className="contactBox">
           <h1>Contact</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <div>
-
+            <div>
+              <form>
+                
+              </form>
+            </div>
+            <div>
+              <img className="contactPic" src="../images/charleston_map.png"/>
+            </div>
           </div>
         </div>
         <div className="quoteBox"></div>
